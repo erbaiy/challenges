a5d2d04dccad823174a91ffd46a79143e0b85af9
erbaiy <younesserbai@gmail.com>
2023-09-14 16:27:00 +0200

    third challenge

---
+++ challenge3/main.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+
+int main()
+{
+    int a,b,r;
+
+    printf("entre le premier nember a \n");
+    scanf(" %d",&a);
+
+    printf("entre le premier nember b \n");
+
+    scanf(" %d",&b);
+
+    printf("a +  b = %d\n ",a+b);
+    printf("a - b = %d\n ",a-b);
+    printf("a * b = %d\n ",a*b);
+    printf("a %% b = %d\n ",a%b);
+    printf("a /b = %d"  ,a/b);
+
+          return 0;
+}
