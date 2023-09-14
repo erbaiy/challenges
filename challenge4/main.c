fa7fa5d991f8b3cf0d30014cd2b2ae22f0dc2317
erbaiy <younesserbai@gmail.com>
2023-09-14 17:49:29 +0200

    Added my project

---
+++ challenge4/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int a,d,c,b,somme ,moyenne;
+
+
+   printf("entrer a :");
+   scanf("%d",&a);
+
+   printf("entrer d :");
+   scanf("%d",&d);
+
+   printf("entrer b :");
+   scanf("%d ",&b);
+
+   printf("entrer c :");
+   scanf("%d ",&c);
+
+   somme = (a+b+c+d);
+   moyenne = somme/3;
+
+   printf("la sommes de ce nombers est %d \n et la moyenne est %d ",somme ,moyenne);
+
+    return 0;
+}
