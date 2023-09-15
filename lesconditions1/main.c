efebc1a06ee07335e9a5b32eb291e6c052646083
erbaiy <younesserbai@gmail.com>
2023-09-15 15:25:35 +0200

    lesconditions1/main.c

---
+++ lesconditions1/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int x;
+    printf(" entrer un nomber ");
+    scanf("%d",&x);
+    if(x%2==0){
+
+
+
+    printf("le nomer sisaire est paire");}
+
+     else {
+
+        printf("le nomber est impaire");
+
+     }
+
+
+    return 0;
+}
