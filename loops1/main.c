4ce80b37da3a7a5b09a0121b1de2d87694b34aeb
erbaiy <younesserbai@gmail.com>
2023-09-16 16:13:42 +0200

    loops 1

---
+++ loops1/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int x,m,c;
+    printf("enter the the number :");
+    scanf("%d",&m);
+
+    for( x=1;x<=10;x++){
+            c=x*m;
+            printf("%d x %d = %d\n",x,m,c);
+
+    }
+
+
+}
